9a626a664eb2865d3b7922594a1497f69b14a666
trxius <ozanbayaz75@gmail.com>
2026-08-13 22:37:06 +0300

    Add files via upload

--- src/main/java/me/advancedtags/AdvancedTags.java
+++ src/main/java/me/advancedtags/AdvancedTags.java
@@ -1,49 +1,51 @@
-/*
- * AdvancedTags - A modern Minecraft title management system.
- * Copyright (C) 2026 ozan
- * 
- * Licensed under Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
- * You may not use this work for commercial purposes.
- * For more info: https://creativecommons.org/licenses/by-nc/4.0/
- */
 package me.advancedtags;
 
 import me.advancedtags.command.AdminCommand;
-import me.advancedtags.utils.UpdateChecker;
 import me.advancedtags.command.UnvanCommand;
 import me.advancedtags.core.MessageManager;
 import me.advancedtags.core.StorageManager;
 import me.advancedtags.core.TagManager;
 import me.advancedtags.hook.TagPlaceholder;
 import me.advancedtags.menu.MenuListener;
-import org.bukkit.plugin.java.JavaPlugin;
+import me.advancedtags.utils.SchedulerUtils;
+import me.advancedtags.utils.UpdateChecker;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdvancedTags extends JavaPlugin {
     private TagManager tagManager;
     private StorageManager storageManager;
     private MessageManager messageManager;
+    private BukkitAudiences adventure;
+    private UpdateChecker updateChecker;
 
     @Override
     public void onEnable() {
+        this.adventure = BukkitAudiences.create(this);
         int pluginId = 33319;
-        Metrics metrics = new Metrics(this, pluginId);
+        new Metrics(this, pluginId);
 
-        new UpdateChecker(this);
-        
         saveDefaultConfig();
         this.messageManager = new MessageManager(this);
         this.tagManager = new TagManager(this);
         this.storageManager = new StorageManager(this);
         this.storageManager.load();
 
-        getCommand("unvan").setExecutor(new UnvanCommand(this));
+        this.updateChecker = new UpdateChecker(this);
+
+        if (getCommand("unvan") != null) {
+            getCommand("unvan").setExecutor(new UnvanCommand(this));
+        }
         
         AdminCommand adminCommand = new AdminCommand(this);
-        getCommand("advancedtags").setExecutor(adminCommand);
-        getCommand("advancedtags").setTabCompleter(adminCommand);
+        if (getCommand("advancedtags") != null) {
+            getCommand("advancedtags").setExecutor(adminCommand);
+            getCommand("advancedtags").setTabCompleter(adminCommand);
+        }
         
         getServer().getPluginManager().registerEvents(new MenuListener(this), this);
+        getServer().getPluginManager().registerEvents(this.tagManager, this);
 
         if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new TagPlaceholder(this).register();
@@ -52,12 +54,21 @@ public class AdvancedTags extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        if (this.updateChecker != null) {
+            this.updateChecker.shutdown();
+        }
         if (this.storageManager != null) {
             this.storageManager.saveSync();
+        }
+        SchedulerUtils.shutdown();
+        if (this.adventure != null) {
+            this.adventure.close();
+            this.adventure = null;
         }
     }
 
     public TagManager getTagManager() { return tagManager; }
     public StorageManager getStorageManager() { return storageManager; }
     public MessageManager getMessageManager() { return messageManager; }
+    public BukkitAudiences getAdventure() { return adventure; }
 }
